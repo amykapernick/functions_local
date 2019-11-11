364b5428e70c51b217383400e1195722ac451197
Amy Kapernick <amy@aimhigherweb.design>
2019-11-11 11:54:16 +0800

    got sendgrid functions working for emails

---
+++ analytics_emails.js
@@ -0,0 +1,25 @@
+const customers = [
+	{
+		contact: [
+			{
+				name: 'Amy',
+				email: 'amy@aimhigherwebdesign.com.au',
+			},
+		],
+		company: 'AimHigher Web Design',
+		sites: [
+			{
+				name: 'AimHigher',
+				slug: 'aimhigher',
+			},
+			{
+				name: 'Amy Goes to Perth',
+				slug: 'amygoestoperth',
+			},
+		],
+	},
+]
+
+module.exports = {
+	customers,
+}
